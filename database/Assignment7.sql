ec7d5855153e0ff2c9031ff3c580e390e34f68c7
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2022-11-07 17:17:35 -0500

    fixed all tables and added the right data type for each column

--- database/Assignment7.sql
+++ database/Assignment7.sql
@@ -4,12 +4,7 @@ DROP TABLE seasons;
 DROP TABLE educations;
 DROP TABLE COMPETENCIES;
 --CREATING TABLES
-CREATE TABLE COMPETENCIES(
-    comp_id char(4) PRIMARY KEY,
-    COMP_NAME VARCHAR2(60) NOT NULL,
-    SPECIFICATION CHAR(1)NOT NULL,
-    COMP_DESCRIPTION VARCHAR2(1000) NOT NULL
-);
+
 CREATE TABLE seasons(
     season_id Char(1) PRIMARY KEY,
     season_name Varchar2(20) NOT NULL
@@ -33,11 +28,22 @@ CREATE TABLE dawson_courses(
     term_id number REFERENCES term_seasons(term_id)
 );
 CREATE TABLE elements_of_competency(
-    element_id varchar2(2) PRIMARY KEY,
+    element_id varchar2(3) PRIMARY KEY,
     element_name varchar2(100) NOT NULL,
     element_description varchar2(1000),
     comp_id char(4) REFERENCES competencies(comp_id)
 );
+CREATE TABLE competencies(
+    comp_id char(4) PRIMARY KEY,
+    comp_name varchar2(60) NOT NULL,
+    specification char(1)NOT NULL,
+    comp_description varchar2(1000) NOT NULL
+);
+CREATE TABLE element_course(
+    element_id varchar2(3) REFERENCES elements_of_competency(element_id),
+    course_number varchar2(20) REFERENCES dawson_courses(course_number),
+    associated_time number(4,2)
+)
 
 
 INSERT INTO seasons VALUES('1','Fall');
