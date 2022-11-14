5bb860ee7cc4e2f1a680ca59bbfb8ccd303ed277
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2022-11-14 16:34:00 -0500

    remove duplicate table and altered table names in types.sql

--- database/types.sql
+++ database/types.sql
@@ -1,47 +1,44 @@
-CREATE OR REPLACE TYPE season_type AS OBJECT (
+
+CREATE OR REPLACE TYPE season_typ AS OBJECT (
     season_id   CHAR(1),
     season_name VARCHAR2(20)
 );
 /
 
-CREATE OR REPLACE TYPE term_type AS OBJECT (
+CREATE OR REPLACE TYPE TERM_TYP AS OBJECT (
     term_id NUMBER(2, 0),
-    OSeason season_type
+    OSeason season_typ
 );
 /
 
-CREATE OR REPLACE TYPE education_type AS OBJECT (
+CREATE OR REPLACE TYPE education_typ AS OBJECT (
     education_type_id CHAR(1),
     education_type    VARCHAR2(30)
 );
 /
 
-CREATE OR REPLACE TYPE course_type AS OBJECT (
+CREATE OR REPLACE TYPE course_typ AS OBJECT (
     course_number      VARCHAR2(20),
     course_name        VARCHAR2(50),
     course_description VARCHAR2(1000),
     class_hours        NUMBER(2, 0),
     lab_hours          NUMBER(2, 0),
     homework_hours     NUMBER(2, 0),
-    OEducation         education_type,
-    OTerm              term_type
-);
-CREATE OR REPLACE TYPE ELEMENT_TYP; AS OBJECT (
-    element_id VARCHAR2(3),
-    element_name VARCHAR2(100),
-    element_description VARCHAR2(1000),
-    OComp COMP_TYP
+    OEducation         education_typ,
+    OTerm              term_typ
 );
 /
-CREATE OR REPLACE TYPE EDUCATION_TYP; AS OBJECT (
-    education_type_id char(1),
-    education_type varchar2(30)
-);
-/
-CREATE OR REPLACE TYPE COMP_TYP; AS OBJECT (
+CREATE OR REPLACE TYPE COMP_TYP AS OBJECT (
     comp_id CHAR(4),
     comp_name VARCHAR2(60),
     specification CHAR(1),
     comp_description VARCHAR2(1000)
+);
+/
+CREATE OR REPLACE TYPE ELEMENT_TYP AS OBJECT (
+    element_id VARCHAR2(3),
+    element_name VARCHAR2(100),
+    element_description VARCHAR2(1000),
+    OComp COMP_TYP
 );
 /
